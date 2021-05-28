22c07c63f9268bc068f4195c5bcefa466c1d9518
Harun ERGUL <harunergul@outlook.com>
2021-05-28 22:48:28 +0300

    fontawesome codes

---
+++ LocantaApp/ViewComponents/RestaurantCountViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using LocantaApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+
+namespace LocantaApp.ViewComponents
+{
+    public class RestaurantCountViewComponent : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+        public RestaurantCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData  =  restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCount();
+            return View(count);
+        }
+    }
+}
