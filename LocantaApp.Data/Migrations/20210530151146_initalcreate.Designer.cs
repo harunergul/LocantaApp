e0adc86c1f51369fb6688f57640ab757cb13ea38
Harun ERGUL <harunergul@outlook.com>
2021-05-30 18:20:34 +0300

    migrations

--- LocantaApp.Data/Migrations/20210530151146_initalcreate.Designer.cs
+++ LocantaApp.Data/Migrations/20210530151146_initalcreate.Designer.cs
@@ -2,14 +2,16 @@
 using LocantaApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LocantaApp.Data.Migrations
 {
     [DbContext(typeof(LocantaAppDbContext))]
-    partial class LocantaAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530151146_initalcreate")]
+    partial class initalcreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LocantaApp.Data/Migrations/20210530151146_initalcreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace LocantaApp.Data.Migrations
+{
+    public partial class initalcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurants",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurants",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Cuisine",
+                table: "Restaurants",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Restaurants",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurants",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurants",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Cuisine",
+                table: "Restaurants",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Restaurants",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
